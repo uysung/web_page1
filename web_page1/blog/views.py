4a3b55abff0efa0ee44c66898a275c4f9cfd27d9
uysung <air2778000@naver.com>
2022-11-21 12:42:19 +0900

    CSV로 블로그 포스트 목록 퓨ㅔ이지 만들기

--- web_page1/blog/views.py
+++ web_page1/blog/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 # Create your views here.
 
-def index(request) :
-    posts = Post.objects.all().order_by('-pk')
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    # template_name = 'blog/index.html'
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts' : posts,
-        }
-    )
+# def index(request) :
+#     posts = Post.objects.all().order_by('-pk')
+
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts' : posts,
+#         }
+#     )
 
 def single_post_page(request, pk) :
     post = Post.objects.get(pk=pk)
