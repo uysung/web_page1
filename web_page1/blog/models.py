7d2ff0338b390604d8ec30f889eefe0a13ba3750
uysung <air2778000@naver.com>
2022-11-30 10:17:50 +0900

    삭제된 작성자는 None으로 표시하기

--- web_page1/blog/models.py
+++ web_page1/blog/models.py
@@ -16,7 +16,7 @@ class Post(models.Model) :
     created_at = models.DateTimeField(auto_now=True)
     update_at = models.DateTimeField(auto_now=True)
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
