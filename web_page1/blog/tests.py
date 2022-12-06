64296872548a54719d667cf4b55292c37e80c3ef
uysung <air2778000@naver.com>
2022-12-06 10:59:01 +0900

    포스트 작성 페이지 접근 권한 설정하기

--- web_page1/blog/tests.py
+++ web_page1/blog/tests.py
@@ -9,6 +9,8 @@ class TestView(TestCase) :
         self.client = Client()
         self.user_trump = User.objects.create_user(username='trump', password='somepassword')
         self.user_obama = User.objects.create_user(username='obama', password='somepassword')
+        self.user_obama.is_staff = True
+        self.user_obama.save
 
         # 카테고리
         self.category_programming = Category.objects.create(name='programming', slug='programming')
@@ -182,9 +184,13 @@ class TestView(TestCase) :
             response = self.client.get('/blog/create_post/')
             self.assertNotEqual(response.status_code, 200)
 
-            # 로그인을 한다.
+            # staff가 아닌 trump가 로그인을 한다.
             self.client.login(username='trump', password='somepassword')
+            response = self.client.get('/blog/create_post/')
+            self.assertNotEqual(response.status_code, 200)
 
+            # staff인 obama로 로그인을 한다.
+            self.client.login(username='obama', password='somepassword')
             response = self.client.get('/blog/create_post/')
             self.assertEqual(response.status_code, 200)
             soup = BeautifulSoup(response.content, 'html.parser')
@@ -204,7 +210,7 @@ class TestView(TestCase) :
             self.assertEqual(Post.objects.count(), 4)
             last_post = Post.objects.last()
             self.assertEqual(last_post.title, "Post Form 만들기")
-            self.assertEqual(last_post.author.username, 'trump')
+            self.assertEqual(last_post.author.username, 'obama')
 
             self.assertEqual(last_post.tags.count(), 3)
             self.assertTrue(Tag.objects.get(name='new tag'))
--- web_page1/blog/views.py
+++ web_page1/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from . models import Post, Category, Tag
 
 # Create your views here.
@@ -23,7 +23,7 @@ class PostDetail(DetailView) :
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
-class PostCreate(LoginRequiredMixin, CreateView):
+class PostCreate(LoginRequiredMixin,UserPassesTestMixin, CreateView):
     model = Post
     fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
@@ -32,7 +32,7 @@ class PostCreate(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         current_user = self.request.user
-        if current_user.is_authenticated:
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
             form.instance.author = current_user
             return super(PostCreate, self).form_valid(form)
         else:
