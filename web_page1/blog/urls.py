b4b657bc8ef7901ea7c1287980df7771724dd469
uysung <air2778000@naver.com>
2022-12-06 12:03:53 +0900

    django-crispy-forms 적용하기

--- web_page1/blog/urls.py
+++ web_page1/blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 이 부분을 채울 겁니다!
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>/', views.tag_page),
